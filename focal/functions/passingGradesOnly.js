94b72ca94c9b3968c4ebee52823fb4f8cf92e50f
Amina Malik <amina.malik@telus.com>
2022-11-17 21:57:50 -0500

    added more content for understanding

---
+++ focal/functions/passingGradesOnly.js
@@ -0,0 +1,10 @@
+function isBigEnough(value) {
+  return value >= 70;
+}
+
+// const filtered = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35].filter(isBigEnough);
+// console.log(filtered);
+// const array1 = [1, 4, 9, 16];
+// //pass a function to the map. why? map() creates a new array with the results of callng a provided funtion on every element in the calling array
+// const map1 = array1.map(x => x*2); //(x) => { return (x * 2); } or function (x) { return (x * 2); }, These are all different ways of creating the same anonymous callback function 
+// console.log(map1);
